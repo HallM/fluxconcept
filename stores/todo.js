6fda85d6adf124456ad6585df78aadbbeec277dd
Matthew Hall <mdhall@gmail.com>
2015-08-16 13:53:21 -0500

    Adjust return for store functions

--- stores/todo.js
+++ stores/todo.js
@@ -7,8 +7,10 @@ export default {
       // the data is contained by the library, but may be used in any way
       // may use immutable-style functions and even Immutable libs as well
       data = data.concat(text);
-      // return true if the data changed, this will emit change to listeners
-      return true;
+      // return the latest copy of the data if there is a change
+      // if a non-undefined thing is returned, that is the new data
+      // and listeners will be notified of a change
+      return data;
     },
 
     changeTodo(context, data, index, text) {
@@ -16,20 +18,19 @@ export default {
         // immutabile style is not required
         // data may be manipulated however you wish
         data[index] = text;
-        return true;
+        return data;
       } else {
-        // return false if the data didn't change, doesn't emit change
-        return false;
+        // return undefined or don't return (implicit undefined) for no-change
+        return undefined;
       }
     },
 
     removeTodo(context, data, index) {
       if (index >= 0 && data.length < index) {
         data = data.splice(index, 1);
-        return true;
-      } else {
-        return false;
+        return data;
       }
+      // we can also have no return to signify no change
     }
   },
 
